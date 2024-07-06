71202084f7d9302d029f9080b10155310f2317f6
jsut21 <94725979+jsut21@users.noreply.github.com>
2024-07-06 16:46:20 +0900

    Title: 색종이의 총 넓이, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240706/색종이의 총 넓이/the-total-area-of-colored-paper..java
+++ 240706/색종이의 총 넓이/the-total-area-of-colored-paper..java
@@ -11,6 +11,8 @@ public class Main {
         for(int i = 0; i<n; i++){
             int x = sc.nextInt();
             int y = sc.nextInt();
+            x+=100;
+            y+=100;
 
             for(int j = x; j <x+8; j++){
                 for(int k = y; k<y+8; k++){
